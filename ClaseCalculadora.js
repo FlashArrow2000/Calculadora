314137a996c30d969774ba38b9c0e8306ce94a74
FlashArrow2000 <47357710+FlashArrow2000@users.noreply.github.com>
2019-02-19 16:28:58 +0100

    Add files via upload

--- ClaseCalculadora.js
+++ ClaseCalculadora.js
@@ -7,15 +7,68 @@ class Calculadora{ //declaramos la clase o molde para pbjeto
     suma(){//funcion atributo 
         return parseInt(this.Numero1) + parseInt(this.Numero2);//retorna el resultado
     }
+    //PROMESA SUMA ASINCRONA
+    SumaAsincrona(){
+        var objeto = this; //aqui toma todo lo de la clase una vez ya instansciada
+        
+            return new Promise(function(resolve,reject){//promesa
+                                                        try{//intenta hacer la suma
+                                                            resolve(parseInt(objeto.Numero1) + parseInt(objeto.Numero2));
+                                                            }
+                                                catch(err){//si no hace la suma, pasa lo siguiente:
+                                                            reject(err.message);
+                                                            }
+            });
+    }
     resta(){//funcion atributo 
         return parseInt(this.Numero1) - parseInt(this.Numero2);//retorna el resultado
+    }
+    //PROMESA RESTA ASINCRONA
+    RestaAsincrona(){
+        var objeto = this; //aqui toma todo lo de la clase una vez ya instansciada
+        
+            return new Promise(function(resolve,reject){//promesa
+                                                        try{//intenta hacer la resta
+                                                            resolve(parseInt(objeto.Numero1) - parseInt(objeto.Numero2));
+                                                            }
+                                                catch(err){//si no hace la suma, pasa lo siguiente:
+                                                            reject(err.message);
+                                                            }
+            });
     }
     multiplicacion(){//funcion atributo 
         return parseInt(this.Numero1) * parseInt(this.Numero2);//retorna el resultado
     }
+    //PROMESA MULTIPLICACION ASINCRONA
+    MultiplicacionAsincrona(){
+        var objeto = this; //aqui toma todo lo de la clase una vez ya instansciada
+        
+            return new Promise(function(resolve,reject){//promesa
+                                                        try{//intenta hacer la multiplicacion
+                                                            resolve(parseInt(objeto.Numero1) * parseInt(objeto.Numero2));
+                                                            }
+                                                catch(err){//si no hace la suma, pasa lo siguiente:
+                                                            reject(err.message);
+                                                            }
+            });
+    }
     division(){//funcion atributo 
         return parseInt(this.Numero1) / parseInt(this.Numero2);//retorna el resultado
     }
+    //PROMESA DIVISION ASINCRONA
+    DivisionAsincrona(){
+        var objeto = this; //aqui toma todo lo de la clase una vez ya instansciada
+        
+            return new Promise(function(resolve,reject){//promesa
+                                                        try{//intenta hacer la division
+                                                            resolve(parseInt(objeto.Numero1) / parseInt(objeto.Numero2));
+                                                            }
+                                                catch(err){//si no hace la suma, pasa lo siguiente:
+                                                            reject(err.message);
+                                                            }
+            });
+    }
+
 }
 //Proceso de calculo
 //Declaramos variables globales
@@ -36,16 +89,36 @@ function igual(){
     let claseinstanciada = new Calculadora(G_Numero1,G_Numero2);
     switch (G_Signo){
     case '+':
-    document.getElementById("resultado").value = claseinstanciada.suma();
+    //document.getElementById("resultado").value = claseinstanciada.suma();
+            claseinstanciada.SumaAsincrona().then(function(response){
+                document.getElementById('resultado').value = response;//manda el resultado
+            }, function(error){//manda el error
+                document.getElementById("No se pudo");
+            });
     break;
     case '-':
-    document.getElementById("resultado").value = claseinstanciada.resta();
+    //document.getElementById("resultado").value = claseinstanciada.resta();
+            claseinstanciada.RestaAsincrona().then(function(response){
+                document.getElementById('resultado').value = response;
+            }, function(error){
+                document.getElementById("No se pudo");
+            });
     break;
     case '*':
-    document.getElementById("resultado").value = claseinstanciada.multiplicacion();
+    //document.getElementById("resultado").value = claseinstanciada.multiplicacion();
+            claseinstanciada.MultiplicacionAsincrona().then(function(response){
+                document.getElementById('resultado').value = response;
+            }, function(error){
+                document.getElementById("No se pudo");
+            });
     break;
     case '/':
-    document.getElementById("resultado").value = claseinstanciada.division();
+    //document.getElementById("resultado").value = claseinstanciada.division();
+            claseinstanciada.DivisionAsincrona().then(function(response){
+                document.getElementById('resultado').value = response;
+            }, function(error){
+                document.getElementById("No se pudo");
+            });
     break;
     }
 }
